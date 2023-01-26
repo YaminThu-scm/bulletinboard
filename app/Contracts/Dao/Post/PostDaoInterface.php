da4fa94863e1706da097d1d47ea8e7cf58d188bd
Yaminthu <scm.yaminthu@gmail.com>
2023-01-26 10:59:51 +0630

    dao frame create

---
+++ app/Contracts/Dao/Post/PostDaoInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Contracts\Dao\Post;
+
+/**
+ * Interface for Data Accessing Object of Post
+ */
+interface PostDaoInterface
+{
+}
---
+++ app/Dao/Post/PostDao.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Dao\Post;
+
+use App\Contracts\Dao\Post\PostDaoInterface;
+
+/**
+ * Data accessing object for post
+ */
+class PostDao implements PostDaoInterface
+{
+}
